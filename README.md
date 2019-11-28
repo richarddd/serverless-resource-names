a19b4381e53e7eaf0698d3a2ec745fd27effcf67
Richard Davison <richard.davison1@gmail.com>
2019-11-28 12:02:14 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,106 @@
+Serverless Resource Names Plugin
+=============================
+This plugin generate resources names from your resource references in `Resources:`
+
+## Setup
+
+Install via npm or yarn in the root of your Serverless project:
+```
+npm install serverless-resource-names --save-dev
+```
+or
+```
+yarn add serverless-resource-names --dev
+```
+
+## Why?
+
+Suppose you have one or many resources in your `serverless.yml` like so:
+
+```yml
+Resources:
+    SomeQueueOne:
+      Type: AWS::SQS::Queue
+      Properties:
+        QueueName: ${self:service.name}-some-queue-one-${self:provider.stage}
+        ...
+
+    SomeQueueTwo:
+      Type: AWS::SQS::Queue
+      Properties:
+        QueueName: ${self:service.name}-some-queue-two-${self:provider.stage}
+        ...
+
+    UploadBucket:
+      Type: AWS::S3::Bucket
+      Properties:
+        BucketName: ${self:service.name}-upload-bucket-${self:provider.stage}
+        ...
+```
+
+**Notice how you have to constantly specify names for these services?**
+
+Futhermore you're likley to use them in your enviroment variables as well
+
+```yml
+provider:
+    environment:
+        SOME_QUEUE_ONE: ${self:service.name}-some-queue-one-${self:provider.stage}
+        SOME_QUEUE_TWO: ${self:service.name}-some-queue-two-${self:provider.stage}
+        UPLOAD_BUCKET: ${self:service.name}-upload-bucket-${self:provider.stage}
+
+```
+
+This becomes very troublesome to manage when your resources grow and you'll constantly need to add these things
+
+## What does it do?
+
+To solve this problem this plugin generates the eqvivalent resources names by looking at the reference names in you're `serverless.yml`
+
+So now you can omit the `BucketName` (or other name properties for other resources)
+
+So the following resource...
+
+```yml
+UploadBucket:
+    Type: AWS::S3::Bucket
+    Properties:
+        ...
+```
+
+...will generate the following based on the reference `UploadBucket`:
+
+```yml
+UploadBucket:
+    Type: AWS::S3::Bucket
+    Properties:
+        BucketName: service-name-upload-bucket-dev
+
+```
+
+```yml
+functions:
+    func1:
+        environment:
+            UPLOAD_BUCKET: service-name-upload-bucket-dev
+            ...
+        ...
+```
+
+## Settings
+
+You can change the prefix for your resources using
+
+```yml
+custom:
+    resourceNames:
+        prefix: "awesome-prefix"
+       
+```
+
+Default will prefix using your service name in `serverless.yml`
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const TYPE_TO_PROPERTY_NAME = {
+  "AWS::SQS::Queue": "QueueName",
+  "AWS::S3::Bucket": "BucketName",
+  "AWS::SNS::Topic": "TopicName",
+  "AWS::DocDB::DBCluster": "DBClusterIdentifier",
+  "AWS::DynamoDB::Table": "TableName",
+  "AWS::DocDB::DBInstance": "DBInstanceIdentifier",
+  "AWS::EC2::Instance": (name, properties) => {
+    const existingTags = properties.Tags || [];
+    for (const tag of existingTags) {
+      if (tag.Key === "Name") {
+        return;
+      }
+    }
+    properties.Tags = [{ Key: "Name", Value: name }, ...existingTags];
+  }
+};
+
+const convertCase = (text, separator = "_") => {
+  const isCapital = char => char.charCodeAt() >= 65 && char.charCodeAt() <= 90;
+  return text
+    .replace(/[\w]([A-Z])/g, m => {
+      if (isCapital(m[0]) && isCapital(m[1])) {
+        return `${m[0]}${m[1]}${separator}`;
+      }
+      return `${m[0]}${separator}${m[1]}`;
+    })
+    .toLowerCase();
+};
+
+class ResourceNamePlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.provider = this.serverless.getProvider("aws");
+
+    this.hooks = {
+      "before:offline:start:init": this.writeNames.bind(this),
+      "before:aws:common:validate:validate": this.writeNames.bind(this)
+    };
+  }
+
+  setResourceName(acc, name, type, properties) {
+    const { prefix } = this.serverless.service.custom.resourceNames || {
+      prefix: this.serverless.service.name
+    };
+    const stage = this.serverless.service.provider.stage;
+
+    const envName = convertCase(name).toUpperCase();
+
+    this.serverless.cli.log(`    ✔ Exposing env ${envName}`);
+
+    const resoureceName = `${prefix}-${envName.replace(
+      /_/g,
+      "-"
+    )}-${stage}`.toLowerCase();
+
+    const nameConverter = TYPE_TO_PROPERTY_NAME[type];
+    if (!nameConverter) {
+      throw Error(`Missing nameconverter for ${type}`);
+    }
+    if (nameConverter instanceof Function) {
+      nameConverter(resoureceName, properties);
+    } else {
+      properties[nameConverter] = resoureceName;
+    }
+    acc[envName] = resoureceName;
+  }
+
+  writeNames() {
+    this.serverless.cli.log(`Applying resource names...`);
+    const resouceNamesEnvironment = Object.entries(
+      this.serverless.service.resources.Resources
+    ).reduce((acc, [key, resource]) => {
+      this.setResourceName(acc, key, resource.Type, resource.Properties);
+      return acc;
+    }, {});
+    Object.entries(this.serverless.service.functions).forEach(
+      ([name, func]) => {
+        func.environment = { ...func.environment, ...resouceNamesEnvironment };
+      }
+    );
+  }
+}
+module.exports = ResourceNamePlugin;
